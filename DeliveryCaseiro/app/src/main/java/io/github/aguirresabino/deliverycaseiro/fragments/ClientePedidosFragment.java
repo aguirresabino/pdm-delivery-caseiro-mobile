5120872fd4e18239e6a167f339973cb8fd934457
Aguirre Sabino <aguirresabino187@gmail.com>
2018-12-21 12:18:26 -0300

    Implementando tela meus pedidos.

--- DeliveryCaseiro/app/src/main/java/io/github/aguirresabino/deliverycaseiro/fragments/ClientePedidosFragment.java
+++ DeliveryCaseiro/app/src/main/java/io/github/aguirresabino/deliverycaseiro/fragments/ClientePedidosFragment.java
@@ -1,12 +1,20 @@
 package io.github.aguirresabino.deliverycaseiro.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import io.github.aguirresabino.deliverycaseiro.R;
+import io.github.aguirresabino.deliverycaseiro.activity.ChefeActivity;
+import io.github.aguirresabino.deliverycaseiro.adapter.ListCardAdapter;
 import io.github.aguirresabino.deliverycaseiro.fragments.base.BaseFragment;
 
 public class ClientePedidosFragment extends BaseFragment {
@@ -14,6 +22,7 @@ public class ClientePedidosFragment extends BaseFragment {
     private final String TAG = getClass().getName() + " ESPECIFICA ";
 
     private Toolbar toolbar;
+    private RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,9 +42,31 @@ public class ClientePedidosFragment extends BaseFragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = view.findViewById(R.id.fragmentClientePedidosRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(new ListCardAdapter(getResources().getStringArray(R.array.teste), this.onClickPedido()));
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
         ////Atualizando a toolbar na implementação do menu lateral
         activityContext.updateToolbarInDrawer(toolbar);
+    }
+
+    private ListCardAdapter.CardOnClickListener onClickPedido() {
+        return new ListCardAdapter.CardOnClickListener() {
+            @Override
+            public void onClickCard(View view, int idx) {
+                String item = getResources().getStringArray(R.array.teste)[idx];
+                Intent intent = new Intent(getActivity(), ChefeActivity.class);
+                startActivity(intent);
+            }
+        };
     }
 }
