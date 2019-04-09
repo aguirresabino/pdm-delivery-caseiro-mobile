e051d2689a70bab4e9f5d744888c34df416704e6
Aguirre Sabino <aguirresabino187@gmail.com>
2019-04-09 03:43:56 -0300

    Implementando uso da câmera para alterar foto de perfil do usuário

--- DeliveryCaseiro/app/src/main/java/io/github/aguirresabino/deliverycaseiro/model/enums/ValuesApplicationEnum.java
+++ DeliveryCaseiro/app/src/main/java/io/github/aguirresabino/deliverycaseiro/model/enums/ValuesApplicationEnum.java
@@ -3,8 +3,8 @@ package io.github.aguirresabino.deliverycaseiro.model.enums;
 public enum ValuesApplicationEnum {
 
 //    URL_BASE_DELIVERY_SERVICE("https://comida-caseira.herokuapp.com/comida-caseira/"),
-    URL_BASE_DELIVERY_SERVICE("http://10.0.3.2:3000/"),
-//    URL_BASE_DELIVERY_SERVICE("http://192.168.0.102:3000/"),
+//    URL_BASE_DELIVERY_SERVICE("http://10.0.3.2:3000/"),
+    URL_BASE_DELIVERY_SERVICE("http://192.168.0.102:3000/"),
     MY_TAG("DELIVERY_CASEIRO_DEBUG");
 
     private String value;
--- DeliveryCaseiro/app/src/main/java/io/github/aguirresabino/deliverycaseiro/view/activity/UsuarioPerfilActivity.java
+++ DeliveryCaseiro/app/src/main/java/io/github/aguirresabino/deliverycaseiro/view/activity/UsuarioPerfilActivity.java
@@ -1,19 +1,33 @@
 package io.github.aguirresabino.deliverycaseiro.view.activity;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -43,6 +57,10 @@ public class UsuarioPerfilActivity extends BaseActivity {
     @BindView(R.id.activityClientePerfilButtonDeletar) AppCompatButton deletar;
     @BindView(R.id.activityClientePerfilAppCompatImageView) AppCompatImageView appCompatImageView;
     @BindView(R.id.activityClientePerfilAlterarFoto) CardView cardAlterarFoto;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    static String mCurrentPhotoPath;
 
     private UsuarioService usuarioService;
     private UsuarioPerfilActivity.LocalBroadcastReceiverUpdate localBroadcastReceiverUpdate;
@@ -100,9 +118,58 @@ public class UsuarioPerfilActivity extends BaseActivity {
         LocalBroadcastManager.getInstance(UsuarioPerfilActivity.this).unregisterReceiver(localBroadcastReceiverDelete);
     }
 
+    private void getPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+        else dispatchTakePictureIntent();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) dispatchTakePictureIntent();
+                else ToastHelper.toastShort(UsuarioPerfilActivity.this, "É necessário dar a permissão para tirar a foto!");
+                return;
+            }
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                photoFile = File.createTempFile("PHOTOAPP", ".jpg", storageDir);
+                mCurrentPhotoPath = "file:" + photoFile.getAbsolutePath();
+            }
+            catch(IOException ex){
+                ToastHelper.toastShort(UsuarioPerfilActivity.this, "Erro ao tirar a foto");
+            }
+
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            DeliveryApplication.usuarioLogado.setImagem(mCurrentPhotoPath);
+            Picasso.get().load(new File(mCurrentPhotoPath)).transform(new CircleTransform()).into(appCompatImageView);
+        }
+    }
+
     @OnClick(R.id.activityClientePerfilAlterarFoto)
-    public void setCardAlterarFoto() {
-        MyLogger.logInfo(ValuesApplicationEnum.MY_TAG.getValue(), UsuarioPerfilActivity.class, "Clicou no CardView Alterar Foto");
+    public void cardViewAlterarFoto() {
+        getPermissions();
     }
 
     @OnClick(R.id.activityClientePerfilButtonDeletar)
